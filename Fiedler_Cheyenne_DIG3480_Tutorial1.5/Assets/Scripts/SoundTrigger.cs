af9dc7d2aa4d8500ad7a182640274aa37be253ac
geistpolter <54725759+geistpolter@users.noreply.github.com>
2019-10-03 19:28:33 -0400

    Tutorial1.5
    Yeehaw.

---
+++ Fiedler_Cheyenne_DIG3480_Tutorial1.5/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public AudioClip musicClipWalk;
+
+    public AudioClip musicClipRun;
+
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        //Makes walk music play
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipWalk;
+            musicSource.Play();
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        //Makes run music play
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipRun;
+            musicSource.Play();
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        //The loopty loop
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+    }
+}
